6b05508b9487be0c2ba4fc0b1bed23343108316d
Matthew Anders <manderson0182@gmail.com>
2025-09-13 23:26:53 -0400

    update sagger

--- backend/swagger.js
+++ backend/swagger.js
@@ -5,11 +5,11 @@ const doc = {
         title: 'My API',
         description: 'Contacts API'
     },
-    host: 'https://cse341-o8bq.onrender.com',
+    host: 'cse341-o8bq.onrender.com',
     schemes: '[https]'
 }
 
 const outputFile = './swagger.json';
 const endpointFiles = ['./routes/index.js'];
 
-//swaggerAutogen(outputFile, endpointFiles, doc);
+swaggerAutogen(outputFile, endpointFiles, doc);
