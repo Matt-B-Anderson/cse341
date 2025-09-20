df70cc8e6c6f13a000b05137010250feec9225c0
Matthew Anders <manderson0182@gmail.com>
2025-09-20 14:13:36 -0400

    fixed issue with router

--- movie-ratings/routes/movieRating.js
+++ movie-ratings/routes/movieRating.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express.router();
+const router = express.Router();
 const movieRatingController = require('../controllers/movieRating');
 
 router.get('/', movieRatingController.getAll);
